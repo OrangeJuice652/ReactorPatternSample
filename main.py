f6208d151a7a4d0f75dd91630d45ed80745dc49a
OrangeJuice652 <miyamizu328@outlook.jp>
2025-07-19 17:53:40 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
+from .reactor import Reactor
+from .eventhandler import (
+    ListnerSocket,
+    EchoSocket,
+)
+
+            
 def main():
     print("Hello from reactorpatternsample!")
 
 
 if __name__ == "__main__":
-    main()
+    reactor = Reactor(
+        ListnerSocket()
+    )
+    for result in reactor.event_loop():
+        if isinstance(result, EchoSocket):
+            # regist echo socket for the reactor
+            reactor.regist_new_event_handler(
+                result
+            )
